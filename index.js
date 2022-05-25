ea0cd07ea68ea204958dd2bf5672b654164db2ad
Aniket Bindhani <aniketbindhani44@gmail.com>
2022-05-25 19:37:18 +0530

    files added

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("Hello world");
+// alert("me");
+
+// var number1 = 34;
+// var number2 = 56;
+// console.log(number1 + number2);
+
+let elem = document.getElementById('click');
+console.log(elem);
+
+let elemClass = document.getElementsByClassName('container');
+console.log(elemClass);
+elemClass[0].getElementsByClassName.background = "yellow";
